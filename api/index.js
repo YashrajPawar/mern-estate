5d398c6d6c5e6f763f60585a6bf5a87992ef7f02
YashrajPawar <yashrajpawar33@gmail.com>
2023-10-20 23:53:20 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
@@ -17,3 +18,6 @@ mongoose
 app.listen(8080, () => {
   console.log("server running on port 8080!!");
 });
+
+
+app.use('/api/user',userRouter);
